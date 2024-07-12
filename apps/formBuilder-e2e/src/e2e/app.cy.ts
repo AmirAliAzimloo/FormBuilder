c7f85237444a06945c4292c89239037bbe8ae3b1
Amir Ali <amiraliazimloo123@gmail.com>
2024-07-12 12:22:12 -0700

    Refactor the logic of e2e testing.

--- apps/formBuilder-e2e/src/e2e/app.cy.ts
+++ apps/formBuilder-e2e/src/e2e/app.cy.ts
@@ -1,9 +1,9 @@
 import { getGreeting } from '../support/app.po';
 
 describe('formBuilder-e2e', () => {
-  beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
+    cy.visit('/')
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
