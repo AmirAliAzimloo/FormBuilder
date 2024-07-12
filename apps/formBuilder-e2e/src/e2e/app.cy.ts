730441ef4fd11fb6f71ccbcaf96f05203fe0ab15
Amir Ali <amiraliazimloo123@gmail.com>
2024-07-12 14:56:41 -0700

    Refactor the CI pipeline to fix the github action issue.

--- apps/formBuilder-e2e/src/e2e/app.cy.ts
+++ apps/formBuilder-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,11 @@
 import { getGreeting } from '../support/app.po';
 
 describe('formBuilder-e2e', () => {
-  beforeEach(() =>{
-     cy.visit('/',{failOnStatusCode:false});
-  });
+  beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains(/Welcome/);
