c93f05f093c15191ac1fd4048954b2a107fcabf1
Amir Ali <amiraliazimloo123@gmail.com>
2024-09-20 16:49:26 +0330

    Add the TWA config and generated TWA app.

---
+++ app/src/main/java/app/vercel/form_builder_sigma_five/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package app.vercel.form_builder_sigma_five.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
